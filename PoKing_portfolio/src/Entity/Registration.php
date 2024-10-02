787a51c449ef131b1c407b81787e4900a65bd0f0
git-alexis <alex59.billemont@gmail.com>
2024-10-02 15:33:33 +0200

    add registration entity class with getter and setter method for all attribut

---
+++ PoKing_portfolio/src/Entity/Registration.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RegistrationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RegistrationRepository::class)]
+class Registration
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'registrations')]
+    private ?event $event = null;
+
+    #[ORM\ManyToOne(inversedBy: 'registrations')]
+    private ?user $user = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $registrationDate = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $registrationTime = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEvent(): ?event
+    {
+        return $this->event;
+    }
+
+    public function setEvent(?event $event): static
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    public function getUser(): ?user
+    {
+        return $this->user;
+    }
+
+    public function setUser(?user $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getRegistrationDate(): ?\DateTimeInterface
+    {
+        return $this->registrationDate;
+    }
+
+    public function setRegistrationDate(\DateTimeInterface $registrationDate): static
+    {
+        $this->registrationDate = $registrationDate;
+
+        return $this;
+    }
+
+    public function getRegistrationTime(): ?\DateTimeInterface
+    {
+        return $this->registrationTime;
+    }
+
+    public function setRegistrationTime(\DateTimeInterface $registrationTime): static
+    {
+        $this->registrationTime = $registrationTime;
+
+        return $this;
+    }
+}
