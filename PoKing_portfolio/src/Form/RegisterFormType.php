83919a78e82ff1d5bd30659d78c8a118327793ae
git-alexis <alex59.billemont@gmail.com>
2024-10-08 16:43:37 +0200

    add form of register with all field ( pseudo, password, name, surname, mail, address, postcode, city and birthday ), their specifications ( authorizes null or not, message if wrong entered and widget for birthday ) and method to set user received data

---
+++ PoKing_portfolio/src/Form/RegisterFormType.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class RegisterFormType extends AbstractType
+{
+    // Method to build the form fields and their configurations
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        // Add 'name' field of type TextType with a custom label
+        $builder
+            ->add('pseudo', TextType::class, [
+                'label' => '* Pseudo',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un pseudo',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => '* Password',
+                'mapped' => false, // Not mapped to the User entity (will be hashed)
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                ],
+            ])
+            ->add('name', TextType::class, [
+                'label' => '* Nom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un nom',
+                    ]),
+                ],
+            ])
+            ->add('surname', TextType::class, [
+                'label' => '* Prénom',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un prénom',
+                    ]),
+                ],
+            ])
+            ->add('mail', EmailType::class, [
+                'label' => '* Adresse e-mail',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une adresse e-mail',
+                    ]),
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Adresse',
+                'required' => false,
+            ])
+            ->add('postcode', TextType::class, [
+                'label' => 'Code postal',
+                'required' => false,
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'required' => false,
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => '* Numéro de téléphone',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un numéro de téléphone',
+                    ]),
+                ],
+            ])
+            ->add('birthday', DateType::class, [
+                'label' => '* Date de naissance',
+                'widget' => 'single_text',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer une date de naissance',
+                    ]),
+                ],
+            ])
+            ->add('register', SubmitType::class, [
+                'label' => 'Register',
+            ])
+        ;
+    }
+
+    // Method to configure options for this form, setting the data_class to User
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
