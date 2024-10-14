334241490e36ab012cfc46cfa2861bec71c8a1d6
git-alexis <alex59.billemont@gmail.com>
2024-10-14 09:43:37 +0200

    add MyAccount controller with route to display the pre-filled form allowing you to modify your personal info, and to delete this account and be redirected to the login page

---
+++ PoKing_portfolio/src/Controller/MyAccountController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\MyAccountFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+#[Route('/my-account')]
+class MyAccountController extends AbstractController
+{
+    #[Route(name: 'app_my_account', methods: ['GET', 'POST', 'PUT'])]
+    public function myAccount(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(MyAccountFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Compte mis à jour avec succès.');
+        }
+
+        return $this->render('my_account/my_account.html.twig', [
+            'myAccountForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/delete', name: 'app_delete_account', methods: ['GET', 'POST', 'DELETE'])]
+    public function deleteAccount(EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+
+        $entityManager->remove($user);
+        $entityManager->flush();
+    }
+}
