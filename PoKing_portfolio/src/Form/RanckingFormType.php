1e20b0d147e516b019cabd1ab39089b25f7f68cc
git-alexis <alex59.billemont@gmail.com>
2024-10-16 18:02:56 +0200

    puts fields in the right order and adds labels and associated const raints on conteunu, input size and type for each field

--- PoKing_portfolio/src/Form/RanckingFormType.php
+++ PoKing_portfolio/src/Form/RanckingFormType.php
@@ -9,25 +9,75 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class RanckingFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('season')
-            ->add('label')
-            ->add('points')
-            ->add('rankingPosition')
-            ->add('eliminatedBy')
-            ->add('killsNumber')
             ->add('event', EntityType::class, [
                 'class' => Event::class,
-                'choice_label' => 'id',
+                'choice_label' => function (Event $event) {
+                    return $event->getLabel();
+                },
+            ])
+            ->add('season', TextType::class, [
+                'label' => '* Season ',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9\'._\- ]+$/',
+                        'message' => 'Caractères autorisés : a-z, A-Z, 0-9, \', \'.\', \'_\', \'-\' et \' \'',
+                    ]),
+                    new Length(['min' => 1, 'minMessage' => 'La saison doit être renseignée']),
+                ],
+            ])
+            ->add('label', TextType::class, [
+                'label' => '* Label ',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9\'._\- ]+$/',
+                        'message' => 'Caractères autorisés : a-z, A-Z, 0-9, \', \'.\', \'_\', \'-\' et \' \'',
+                    ]),
+                    new Length(['min' => 1, 'minMessage' => 'Le libellé doit être renseigné']),
+                ],
             ])
             ->add('user', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'id',
+                'choice_label' => function (User $user) {
+                    return $user->getPseudo();
+                },
+            ])
+            ->add('rankingPosition', NumberType::class, [
+                'label' => '* Ranking position ',
+                'constraints' => [
+                    new Positive(['message' => 'Le stack doit être un entier positif']),
+                ],
+            ])
+            ->add('points', NumberType::class, [
+                'label' => '* Points ',
+                'constraints' => [
+                    new Positive(['message' => 'Le stack doit être un entier positif']),
+                ],
+            ])
+            ->add('killsNumber', NumberType::class, [
+                'label' => '* Kills number ',
+                'constraints' => [
+                    new Positive(['message' => 'Le stack doit être un entier positif']),
+                ],
+            ])
+            ->add('eliminatedBy', TextType::class, [
+                'label' => '* Eliminated by ',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9._-]+$/',
+                        'message' => 'Caractères autorisés : a-z, A-Z, 0-9, \'.\', \'_\' et \'-\'',
+                    ]),
+                ],
             ])
         ;
     }
