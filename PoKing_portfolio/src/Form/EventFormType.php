9c2677ad91cd4f491bacddb96e9c057f6b39f5dd
git-alexis <alex59.billemont@gmail.com>
2024-10-16 17:16:15 +0200

    puts fields in the right order and adds labels and associated constraints on conteunu, input size and type for each field

--- PoKing_portfolio/src/Form/EventFormType.php
+++ PoKing_portfolio/src/Form/EventFormType.php
@@ -9,51 +9,103 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class EventFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('season')
-            ->add('label')
-            ->add('startingDate', null, [
+            ->add('season', TextType::class, [
+                'label' => '* Season',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9\'._\- ]+$/',
+                        'message' => 'Caractères autorisés : a-z, A-Z, 0-9, \', \'.\', \'_\', \'-\' et \' \'',
+                    ]),
+                    new Length(['min' => 1, 'minMessage' => 'La saison doit être renseignée']),
+                ],
+            ])
+            ->add('label', TextType::class, [
+                'label' => '* Label',
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9\'._\- ]+$/',
+                        'message' => 'Caractères autorisés : a-z, A-Z, 0-9, \', \'.\', \'_\', \'-\' et \' \'',
+                    ]),
+                    new Length(['min' => 1, 'minMessage' => 'Le libellé doit être renseigné']),
+                ],
+            ])
+            ->add('stack', NumberType::class, [
+                'label' => '* Stack',
+                'constraints' => [
+                    new Positive(['message' => 'Le stack doit être un entier positif']),
+                ],
+            ])
+            ->add('comment', TextType::class, [
+                'label' => 'Comment',
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'max' => 1000,
+                        'maxMessage' => 'Maximum 1000 caractères',
+                    ]),
+                ],
+            ])
+            ->add('registrationOpeningDate', DateType::class, [
+                'label' => '* Opening date of registration',
                 'widget' => 'single_text',
             ])
-            ->add('finishDate', null, [
+            ->add('registrationOpeningTime', TimeType::class, [
+                'label' => '* Opening time of registration',
                 'widget' => 'single_text',
             ])
-            ->add('startingTime', null, [
+            ->add('registrationClosingDate', DateType::class, [
+                'label' => '* Closing date of registration',
                 'widget' => 'single_text',
             ])
-            ->add('finishTime', null, [
+            ->add('registrationClosingTime', TimeType::class, [
+                'label' => '* Closing time of registration',
                 'widget' => 'single_text',
             ])
-            ->add('stack')
-            ->add('comment')
-            ->add('registrationOpeningDate', null, [
+            ->add('startingDate', DateType::class, [
+                'label' => '* Opening date of event',
                 'widget' => 'single_text',
             ])
-            ->add('registrationClosingDate', null, [
+            ->add('startingTime', TimeType::class, [
+                'label' => '* Opening time of event',
                 'widget' => 'single_text',
             ])
-            ->add('registrationOpeningTime', null, [
+            ->add('finishDate', DateType::class, [
+                'label' => '* Closing date of event',
                 'widget' => 'single_text',
             ])
-            ->add('registrationClosingTime', null, [
+            ->add('finishTime', TimeType::class, [
+                'label' => '* Closing time of event',
                 'widget' => 'single_text',
             ])
             ->add('place', EntityType::class, [
+                'label' => '* Place',
                 'class' => Place::class,
                 'choice_label' => function (Place $place) {
                     return $place->getName() . ' - ' . $place->getAddress() . ' - ' . $place->getCity() . ' - ' . $place->getPostcode();
-                }
+                },
             ])
             ->add('contact', EntityType::class, [
+                'label' => '* Contact',
                 'class' => Contact::class,
                 'choice_label' => function (Contact $contact) {
                     return $contact->getSurname() . ' - ' . $contact->getName() . ' - ' . $contact->getRole();
-                }
+                },
             ])
         ;
     }
