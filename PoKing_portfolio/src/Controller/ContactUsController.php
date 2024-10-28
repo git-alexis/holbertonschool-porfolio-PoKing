dc0ac1086934df99424616b45b1ac1976fda2605
git-alexis <alex59.billemont@gmail.com>
2024-10-28 15:50:23 +0100

    adds method for sending mail to contact us page

--- PoKing_portfolio/src/Controller/ContactUsController.php
+++ PoKing_portfolio/src/Controller/ContactUsController.php
@@ -5,12 +5,34 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Email;
 
 class ContactUsController extends AbstractController
 {
     #[Route('/contact-us', name: 'app_contact_us')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        if ($request->isMethod('POST')) {
+            $regex = '/^[a-zA-Z0-9._\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/';
+
+            if (preg_match($regex, $request->request->get('email'))) {
+                $email = (new Email())
+                    ->from($request->request->get('email'))
+                    ->to('alex59.billemont@gmail.com')
+                    ->replyTo($request->request->get('email'))
+                    ->subject($request->request->get('subject'))
+                    ->text($request->request->get('message'));
+
+                $mailer->send($email);
+
+                $this->addFlash('success', 'Votre demande a été envoyée avec succès');
+            }
+
+            $this->addFlash('error', 'Votre email est invalide, format valide : test@example.com');
+        }
+
         return $this->render('contact_us/contact_us.html.twig', [
             'controller_name' => 'ContactUsController',
         ]);
