e7920d7a499dc4ae3e892315edc9b8d4e52e8f84
git-alexis <alex59.billemont@gmail.com>
2024-10-31 16:26:09 +0100

    adds a token attribute in the user entity for password reset via e-mail link

--- PoKing_portfolio/src/Entity/User.php
+++ PoKing_portfolio/src/Entity/User.php
@@ -54,6 +54,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'json')]
     private array $roles = [];
 
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $resetToken;
+
     /**
      * @var Collection<int, Rancking>
      */
@@ -217,7 +220,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
+
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
@@ -226,6 +229,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRoles(array $roles): static
     {
         $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getResetToken(): ?string
+    {
+        return $this->resetToken;
+    }
+
+    public function setResetToken(?string $resetToken): static
+    {
+        $this->resetToken = $resetToken;
 
         return $this;
     }
