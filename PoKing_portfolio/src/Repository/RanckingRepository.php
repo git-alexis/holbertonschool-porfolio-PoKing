d98f44f6353e2cbc235aab9e7d5e80bdf1992488
git-alexis <alex59.billemont@gmail.com>
2024-11-17 17:50:56 +0100

    removes commented code

--- PoKing_portfolio/src/Repository/RanckingRepository.php
+++ PoKing_portfolio/src/Repository/RanckingRepository.php
@@ -15,29 +15,4 @@ class RanckingRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Rancking::class);
     }
-
-    //    /**
-    //     * @return Rancking[] Returns an array of Rancking objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Rancking
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
--- PoKing_portfolio/src/Repository/RegistrationRepository.php
+++ PoKing_portfolio/src/Repository/RegistrationRepository.php
@@ -15,29 +15,4 @@ class RegistrationRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Registration::class);
     }
-
-    //    /**
-    //     * @return Registration[] Returns an array of Registration objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('r.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Registration
-    //    {
-    //        return $this->createQueryBuilder('r')
-    //            ->andWhere('r.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
